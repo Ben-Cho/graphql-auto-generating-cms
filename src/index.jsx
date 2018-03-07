a135fd289ba32f98ad87291eb4ee71bff319c032
stevenkaspar <srkaspar@gmail.com>
2018-03-07 14:25:35 -0600

    more version updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Loader, Message, Segment } from 'semantic-ui-react';
-import 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch';
 import Promise from 'promise-polyfill';
 import SideMenu from './components/SideMenu';
 import View from './components/View';
